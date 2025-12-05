75e4927ff0f31aacb85927859d3593c276a7e7b4
chomusuke-png <owokenji@gmail.com>
2025-12-05 11:51:44 -0300

    style improvements

--- includes/class-abh-shortcode.php
+++ includes/class-abh-shortcode.php
@@ -24,14 +24,16 @@ class ABH_Shortcode
             'sunday' => 'Domingo'
         ];
 
-        // Obtenemos la zona horaria configurada en WP o la forzamos a Santiago para mostrar
-        // (Como en Logic forzamos a Santiago, mostraremos eso)
         $timezone_label = "Santiago de Chile";
 
         ob_start();
         ?>
         <div class="abh-widget-card">
             <h4 class="abh-title">Horarios de Atención</h4>
+
+            <div class="abh-timezone-header">
+                <small><i class="dashicons dashicons-globe"></i> Zona: <?php echo $timezone_label; ?></small>
+            </div>
 
             <div class="abh-badge <?php echo $status['class']; ?>">
                 <?php echo $status['msg']; ?>
@@ -43,11 +45,9 @@ class ABH_Shortcode
                 foreach ($days_map as $key => $label):
                     $is_closed = get_option("abh_{$key}_closed");
 
-                    // Turno 1
                     $start1 = get_option("abh_{$key}_start");
                     $end1 = get_option("abh_{$key}_end");
 
-                    // Turno 2
                     $start2 = get_option("abh_{$key}_start_2");
                     $end2 = get_option("abh_{$key}_end_2");
 
@@ -56,12 +56,12 @@ class ABH_Shortcode
                     if ($is_closed) {
                         $time_html = '<span class="closed-text">Cerrado</span>';
                     } elseif ($start1 && $end1) {
-                        // Formato Turno 1
-                        $time_html .= date("H:i", strtotime($start1)) . " - " . date("H:i", strtotime($end1));
+                        // Turno 1
+                        $time_html .= date("H:i", strtotime($start1)) . "-" . date("H:i", strtotime($end1));
 
-                        // Formato Turno 2 (si existe)
+                        // Turno 2 (Al lado, separado por / )
                         if ($start2 && $end2) {
-                            $time_html .= "<br>" . date("H:i", strtotime($start2)) . " - " . date("H:i", strtotime($end2));
+                            $time_html .= ' <span class="separator">/</span> ' . date("H:i", strtotime($start2)) . "-" . date("H:i", strtotime($end2));
                         }
                     } else {
                         $time_html = '<span class="closed-text">Cerrado</span>';
@@ -80,9 +80,6 @@ class ABH_Shortcode
                 <div class="abh-msg"><?php echo esc_html($msg); ?></div>
             <?php endif; ?>
 
-            <div class="abh-timezone">
-                <small><i class="dashicons dashicons-globe"></i> Hora: <?php echo $timezone_label; ?></small>
-            </div>
         </div>
         <?php
         return ob_get_clean();
